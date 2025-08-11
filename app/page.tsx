801cefb3280b59c2bfe1cd1247cc4d70d46d23ee
Mohammad Asif Azimi <a12428349@unet.univie.ac.at>
2025-08-11 15:14:57 +0430

    Add shadcn ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
-export default async function Home() {
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
   return (
     <main>
       <h1>Home page</h1>
+      <Button>Click me</Button>
     </main>
   );
 }
