17cd8baa1fbe81196bdd13b950b99bb5ceb17948
Mohammad Asif Azimi <a12428349@unet.univie.ac.at>
2025-08-16 22:54:38 +0430

    Fix bugs

--- server/actions/create-posts.ts
+++ server/actions/create-posts.ts
@@ -1,40 +1,40 @@
-"use server";
+// "use server";
 
-import { db } from "@/server";
+// import { db } from "@/server";
 
-export type FormState = {
-  success: string | null;
-  error: string | null;
-};
+// export type FormState = {
+//   success: string | null;
+//   error: string | null;
+// };
 
-async function createPost(prevState: FormState, formData: FormData) {
-  const title = formData.get("title")?.toString().trim();
-  if (!title) {
-    return { error: "Title is required!", success: null };
-  }
+// async function createPost(prevState: FormState, formData: FormData) {
+//   const title = formData.get("title")?.toString().trim();
+//   if (!title) {
+//     return { error: "Title is required!", success: null };
+//   }
 
-  const content = formData.get("content")?.toString().trim();
-  if (!content) {
-    return { error: "Content is required!", success: null };
-  }
+//   const content = formData.get("content")?.toString().trim();
+//   if (!content) {
+//     return { error: "Content is required!", success: null };
+//   }
 
-  try {
-    const result = await db
-      .insert(posts)
-      .values({
-        title,
-        content,
-      })
-      .returning();
+//   try {
+//     const result = await db
+//       .insert(posts)
+//       .values({
+//         title,
+//         content,
+//       })
+//       .returning();
 
-    if (!result || result.length === 0) {
-      return { error: "No posts found!", success: null };
-    }
-    return { error: null, success: "Post created successfully!" };
-  } catch (err) {
-    console.error("Error creating post:", err);
-    return { error: "Failed to create post.", success: null };
-  }
-}
+//     if (!result || result.length === 0) {
+//       return { error: "No posts found!", success: null };
+//     }
+//     return { error: null, success: "Post created successfully!" };
+//   } catch (err) {
+//     console.error("Error creating post:", err);
+//     return { error: "Failed to create post.", success: null };
+//   }
+// }
 
-export default createPost;
+// export default createPost;
--- server/actions/get-posts.ts
+++ server/actions/get-posts.ts
@@ -1,15 +1,14 @@
-"use server";
+// "use server";
 
-import { db } from "@/server";
-import { posts } from "@/server/schema";
+// import { db } from "@/server";
+// import { posts } from "@/server/schema"; // Assuming posts is defined in your schema
+// async function getPosts() {
+//   const postsLists = await db.select().from(posts);
 
-async function getPosts() {
-  const postsLists = await db.select().from(posts);
+//   if (!postsLists) {
+//     return { error: "No posts found!" };
+//   }
+//   return { success: postsLists };
+// }
 
-  if (!postsLists) {
-    return { error: "No posts found!" };
-  }
-  return { success: postsLists };
-}
-
-export default getPosts;
+// export default getPosts;
