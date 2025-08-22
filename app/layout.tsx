afb38fa7823bf98ffe57eb700cb3aeb696bdcd3a
Mohammad Asif Azimi <a12428349@unet.univie.ac.at>
2025-08-22 23:09:13 +0430

    Refactor: user-button component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navigation/navbar";
+import { ThemeProvider } from "@/components/Providers/theme-provider";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -19,12 +20,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.variable} px-6 md:px-12 max-w-7xl mx-auto antialiased`}
       >
-        <Navbar />
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Navbar />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- components/navigation/user-button.tsx
+++ components/navigation/user-button.tsx
@@ -12,14 +12,34 @@ import {
 import { LogOut, Moon, Settings, Sun, TruckIcon } from "lucide-react";
 import { Session } from "next-auth";
 import { signOut } from "next-auth/react";
+import { useTheme } from "next-themes";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import { Switch } from "../ui/switch";
 
 const UserButton = ({ user }: Session) => {
+  const { theme, setTheme } = useTheme();
+  const [checked, setChecked] = useState(false);
+  const setSwitchState = () => {
+    switch (theme) {
+      case "dark":
+        return setChecked(true);
+      case "light":
+        return setChecked(false);
+      case "system":
+        return setChecked(false);
+    }
+  };
+
+  useEffect(() => {
+    setSwitchState();
+  }, []);
+
   if (user)
     return (
       <DropdownMenu modal={false}>
         <DropdownMenuTrigger>
-          <Avatar>
+          <Avatar className="w-7 h-7">
             {user.image && (
               <AvatarImage
                 src={user.image}
@@ -39,7 +59,7 @@ const UserButton = ({ user }: Session) => {
           </Avatar>
         </DropdownMenuTrigger>
         <DropdownMenuContent className="w-60 p-6" align="end">
-          <div className="mb-4 p-4 flex flex-col gap-1 items-center bg-primary/10 rounded-lg">
+          <div className="mb-4 p-4 flex flex-col gap-1 items-center bg-primary/10 rounded-lg w-full max-w-xs">
             {user.image && (
               <Image
                 src={user.image}
@@ -49,14 +69,16 @@ const UserButton = ({ user }: Session) => {
                 className="rounded-full"
               />
             )}
-            <p className="font-bold text-xs">{user.name}</p>
-            <span className="text-xs font-medium text-secondary-foreground">
+            <p className="font-bold text-xs w-full text-center truncate ">
+              {user.name}
+            </p>
+            <span className="text-xs font-medium text-secondary-foreground w-full text-center truncate ">
               {user.email}
             </span>
           </div>
           <DropdownMenuLabel>My Account</DropdownMenuLabel>
           <DropdownMenuSeparator />
-          <DropdownMenuItem className=" group py-2 font-medium cursor-pointer transition-all duration-500">
+          <DropdownMenuItem className=" group py-2 font-medium cursor-pointer transition-all duration-500 ease-in-out">
             {" "}
             <TruckIcon
               size={14}
@@ -64,22 +86,44 @@ const UserButton = ({ user }: Session) => {
             />
             My orders
           </DropdownMenuItem>
-          <DropdownMenuItem className="py-2 font-medium cursor-pointer transition-all duration-500 group">
+          <DropdownMenuItem className="py-2 font-medium cursor-pointer transition-all duration-500 group ease-in-out">
             <Settings
               size={14}
               className="mr-2 group-hover:rotate-180 transition-all duration-300 ease-in-out"
             />
             Settings
           </DropdownMenuItem>
-          <DropdownMenuItem className="py-2 font-medium cursor-pointer transition-all duration-500">
-            <div className="flex items-center">
-              <Sun size={14} className="mr-2" />
-              <Moon size={14} className="mr-2" />
-              <p>
-                Theme <span>theme</span>
-              </p>
-            </div>
-          </DropdownMenuItem>
+          {theme && (
+            <DropdownMenuItem className="py-2 font-medium cursor-pointer ease-in-out">
+              <div
+                className="flex items-center group"
+                onClick={(e) => e.stopPropagation()}
+              >
+                <div className="relative flex mr-4">
+                  <Sun
+                    size={14}
+                    className=" group-hover:text-yellow-600 absolute group-hover:rotate-180 dark:scale-0 dark:-rotate-90 transition-all duration-750 ease-in-out"
+                  />
+                  <Moon
+                    size={14}
+                    className="group-hover:text-blue-400   scale-0 rotate-90 dark:rotate-0  dark:scale-100 transition-all ease-in-out duration-750"
+                  />
+                </div>
+                <p className="dark:text-blue-400 mr-3  text-yellow-600">
+                  {theme[0].toUpperCase() + theme.slice(1)} Mode
+                </p>
+                <Switch
+                  className="scale-80"
+                  checked={checked}
+                  onCheckedChange={(e) => {
+                    setChecked((prev) => !prev);
+                    if (e) setTheme("dark");
+                    if (!e) setTheme("light");
+                  }}
+                />
+              </div>
+            </DropdownMenuItem>
+          )}
           <DropdownMenuItem
             className="py-2 focus:bg-destructive/30
              group font-medium cursor-pointer transition-all duration-500"
@@ -87,7 +131,7 @@ const UserButton = ({ user }: Session) => {
           >
             <LogOut
               size={14}
-              className="mr-3 group-hover:scale-90 transition-all duration-300 ease-in-out"
+              className="mr-2 group-hover:scale-90 transition-all duration-300 ease-in-out"
             />
             Sign out
           </DropdownMenuItem>
