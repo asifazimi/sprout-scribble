2cd3ef543215f3a606872a1c9840d632fa6bdd21
Mohammad Asif Azimi <a12428349@unet.univie.ac.at>
2025-08-27 21:21:33 +0430

    Add dashboard navbar

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,54 @@
-const Layout = () => {
-  return <div>Layout</div>;
+import DashboardNav from "@/components/navigation/dashboard-nav";
+import { auth } from "@/server/auth";
+import { BarChart, Package, PenSquare, Settings, Truck } from "lucide-react";
+
+const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
+  const session = await auth();
+
+  const userLinks = [
+    {
+      label: "Orders",
+      path: "/dashboard/orders",
+      icon: <Truck size={16} />,
+    },
+    {
+      label: "Settings",
+      path: "/dashboard/settings",
+      icon: <Settings size={16} />,
+    },
+  ] as const;
+
+  const adminLinks =
+    session?.user?.role === "admin"
+      ? [
+          {
+            label: "Analytics",
+            path: "/dashboard/analytics",
+            icon: <BarChart size={16} />,
+          },
+          {
+            label: "Create",
+            path: "/dashboard/add-product",
+            icon: <PenSquare size={16} />,
+          },
+          {
+            label: "Products",
+            path: "/dashboard/products",
+            icon: <Package size={16} />,
+          },
+        ]
+      : [];
+
+  const allLinks = [...adminLinks, ...userLinks];
+
+  return (
+    <div>
+      {/* links for the dashboard page */}
+      <DashboardNav allLinks={allLinks} />
+      {/* rendering the children of the dashboard page */}
+      {children}
+    </div>
+  );
 };
 
-export default Layout;
+export default DashboardLayout;
