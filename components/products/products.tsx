7bd5a9ae4adcf02c7d10085e2cf256b197800b5d
Mohammad Asif Azimi <a12428349@unet.univie.ac.at>
2025-09-29 19:38:32 +0200

    Add product description

--- components/products/products.tsx
+++ components/products/products.tsx
@@ -10,16 +10,16 @@ type ProductTypes = {
 
 const Products = ({ variants }: ProductTypes) => {
   return (
-    <main className="grid sm:grid-cols-1 md:grid-cols-2 gap-7 lg:grid-cols-4">
+    <main className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-7 ">
       {variants.map((variant) => (
         <Link
           className="py-2"
           key={variant.id}
           href={`/products/${variant.id}?id=${variant.id}&productID=${variant.productID}&price=${variant.product.price}&title=${variant.product.title}&type=${variant.productType}&image=${variant.variantImages[0].url}`}
         >
-          <div className="bg-[#F7F7F7] rounded-md flex items-center justify-center group overflow-hidden">
+          <div className="bg-[#F7F7F7]  rounded-md flex items-center justify-center group overflow-hidden">
             <Image
-              className="object-cover transition-transform duration-300 ease-in-out group-hover:scale-140"
+              className="object-cover my-5 transition-transform duration-300 ease-in-out group-hover:scale-125"
               src={variant.variantImages[0].url}
               width={260}
               height={260}
