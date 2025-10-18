694a919823f831458c572c226ac93e724099a206
Mohammad Asif Azimi <a12428349@unet.univie.ac.at>
2025-10-18 18:26:03 +0200

    Refector reset password

--- lib/base-urls.ts
+++ lib/base-urls.ts
@@ -9,7 +9,7 @@
 
 function getBaseURL() {
   if (typeof window !== "undefined") return "";
-  if (process.env.VERCEL_URL) return `https://${process.env.DOMAIN_URL}`;
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
   return "http://localhost:3000";
 }
 
